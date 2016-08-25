ecb1c2f7b75febd8a6acb7ce29410be49dc0bcee
Arif Hossain <arifcse11@gmail.com>
2016-08-25 17:05:55 +0600

    front home page and admin home page designed

---
+++ admin/index.php
@@ -0,0 +1,48 @@
+<?php include 'include/header.php'?>
+
+   
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+           
+            <!-- Top Menu Items -->
+        
+            
+            <?php include 'include/top_nav.php'?>
+            
+            
+            
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+           
+            
+            
+            <?php include 'include/side_nav.php'?>
+            
+            
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            
+
+                <!-- Page Heading -->
+                
+            
+            <?php include 'include/admin_content.php'?>
+            
+            
+                <!-- /.row -->
+
+         
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+   
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+   <?php include 'include/footer.php'?>
---
+++ admin/users.php
@@ -0,0 +1,76 @@
+<?php include 'include/header.php'?>
+
+
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+
+        <!-- Top Menu Items -->
+
+
+        <?php include 'include/top_nav.php'?>
+
+
+
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+
+
+
+        <?php include 'include/side_nav.php'?>
+
+
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+
+
+
+        <!-- Page Heading -->
+
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Users
+                        <small>Subheading</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li>
+                            <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+                        </li>
+                        <li class="active">
+                            <i class="fa fa-file"></i> Blank Page
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+
+
+        <!-- /.row -->
+
+
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+<?php include 'include/footer.php'?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arif
+ * Date: 8/25/2016
+ * Time: 12:06 PM
+ */
